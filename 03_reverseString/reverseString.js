c526152218a9cae42d9d18cb6177175e002d5903
tushar10dwi <tushar10dwi@gmail.com>
2025-01-05 21:34:23 +0530

    Create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringCharList = [];
+    for (const char of string) {
+        stringCharList.push(char);
+    }
+    reversedList = stringCharList.reverse();
+    reverserdWord = stringCharList.join('');
+    return reverserdWord;
 };
 
 // Do not edit below this line
