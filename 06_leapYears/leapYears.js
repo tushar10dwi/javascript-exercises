71e3a248028e2cdd4217562c73971ba82129c303
tushar10dwi <tushar10dwi@gmail.com>
2025-01-06 18:12:46 +0530

    Pass all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    if (givenYear%4===0 && givenYear%100!=0) {
+        return true;
+    }
+    else if (givenYear%100===0 & givenYear%400===0) {
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
