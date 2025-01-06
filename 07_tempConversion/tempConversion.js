162898d6bb54ba808ad3495034543e98ba111d21
tushar10dwi <tushar10dwi@gmail.com>
2025-01-06 18:47:00 +0530

    Pass all the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+  tempInCelcius = tempInFarenheit*5/9 - 160/9;
+  return parseFloat(tempInCelcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  tempInFarenheit = tempInCelcius*9/5 + 32;
+  console.log(tempInFarenheit.toFixed(1));
+  return parseFloat(tempInFarenheit.toFixed(1));
 };
 
 // Do not edit below this line
