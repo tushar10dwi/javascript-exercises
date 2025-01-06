f07f8f017e4011500d58c8882675d5030c8c7419
tushar10dwi <tushar10dwi@gmail.com>
2025-01-06 17:55:06 +0530

    Pass 4 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    startPoint = Math.min(num1,num2);
+    endPoint = Math.max(num1, num2);
+
+    if (num1>=0 && num2>=0) {
+    
+      let sum=0;
+      for(let i=startPoint; i<=endPoint; i++) {sum+=i;}
+
+    return sum;
+
+    }
+    else return 'ERROR';
 
 };
 
